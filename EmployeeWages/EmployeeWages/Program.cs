6157c2ac6ff981acdfbb9f55102e75403b9e6426
ruchi06 <ruchi06@gmail.com>
2022-11-25 12:18:44 +0530

    New Code

--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -1,15 +1,45 @@
-﻿namespace EmployeeWages
+﻿using EmployeeWages;
+
+namespace Employees
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
-            empWageBuilder.addcompanyEmpWage("Airtel", 30, 30, 100);
-            empWageBuilder.addcompanyEmpWage("TCS", 40, 30, 80);
-            empWageBuilder.addcompanyEmpWage("Wipro", 25, 25, 120);
-            empWageBuilder.addcompanyEmpWage("Cipla", 23, 28, 111);
+            EmpWageBuildArray empWageBuilder = new EmpWageBuildArray();
+
+            Console.WriteLine("Enter i to Enter Company Details or otherwise enter any key : ");
+            var input = Console.ReadLine();
+            while (input == "i")
+            {
+                var obj = UserInput();
+                empWageBuilder.addcompanyEmpWage(obj.company, obj.empRatePerHour, obj.numOfWorkingDays, obj.maxHoursPerMonth);
+                Console.WriteLine("Enter i to Enter Company Details or otherwise enter any key :");
+                input = Console.ReadLine();
+            }
             empWageBuilder.computeEmpWage();
+        }
+        public static CompanyEmpWage UserInput()
+        {
+            Console.WriteLine("Enter Company Name : ");
+            string compName = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee Rate Per Hour : ");
+            int empRatePerHour = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter No of Working Days : ");
+            int numOfDays = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Maximum Hours Per Month : ");
+            int maxWorkingHourPerMonth = Convert.ToInt32(Console.ReadLine());
+
+            return new CompanyEmpWage(compName, empRatePerHour, numOfDays, maxWorkingHourPerMonth)
+            {
+                company = compName,
+                empRatePerHour = empRatePerHour,
+                numOfWorkingDays = numOfDays,
+                maxHoursPerMonth = maxWorkingHourPerMonth
+            };
         }
     }
 }
